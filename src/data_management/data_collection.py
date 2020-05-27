c7583ae7e6c2c15eeb54684c994a71aac0eac06e
Niklas Landerer <n.landerer@tum.de>
2020-05-27 18:05:45 +0200

    dc_more sample methods

--- src/data_management/data_collection.py
+++ src/data_management/data_collection.py
@@ -4,5 +4,8 @@
 class DataCollection():
     '''This class is responsible for collecting raw data from div databases or API`s'''
 
-    def get_raw_data(self):
+    def return_raw_data(self):
+        '''Most important method of cls DataCollection returns the raw data as pandas frame'''
+
+    def parse_data_from_online_source(self):
         '''Most important method of cls DataCollection returns the raw data as pandas frame'''
