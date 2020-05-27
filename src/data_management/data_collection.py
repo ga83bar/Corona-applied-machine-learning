e2bf295958daec0ad812bfab274692929f41df19
Niklas Landerer <n.landerer@tum.de>
2020-05-27 14:35:39 +0200

    pep8

--- src/data_management/data_collection.py
+++ src/data_management/data_collection.py
@@ -1,10 +1,9 @@
 """collects data"""
 
+
 class DataCollection():
     '''This class is responsible for collecting raw data from div databases or API`s'''
 
     def getRawData(self):
         '''Most important method of cls DataCollection returns the raw data as pandas frame'''
         pass
-
-    
