fc47f0d7927d6d6e5b243826576fd3b9d533c892
Niklas <niklas@Niklass-MacBook-Pro.local>
2020-06-18 23:15:29 +0200

    vpn_destinations

---
+++ src/data_collection/social_crawler/literals.py
@@ -0,0 +1,19 @@
+"""literals needed for social_crawler"""
+
+VPN_DESTINATIONS = {'europe': ['Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus',
+                                     'Belgium', 'Bosnia & Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus',
+                                     'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Georgia',
+                                     'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Isle of Man', 'Italy',
+                                     'Jersey', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta', 'Moldova',
+                                     'Monaco', 'Montenegro', 'Netherlands', 'North Macedonia', 'Norway', 'Poland',
+                                     'Portugal', 'Romania', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden',
+                                     'Switzerland', 'Turkey', 'Ukraine', 'United Kingdom'],
+
+                          'asia': ['Australia', 'Bangladesh', 'Bhutan', 'Brunei Darussalam', 'Cambodia', 'Hong Kong',
+                                   'India', 'Indonesia', 'Japan', 'Kazakhstan', 'Kyrgyzstan', 'Laos', 'Macau', 'Malaysia',
+                                   'Mongolia', 'Myanmar', 'Nepal', 'New Zealand', 'Pakistan', 'Philippines', 'Singapore',
+                                   'South Korea', 'Sri Lanka', 'Taiwan', 'Thailand', 'Uzbekistan', 'Vietnam'],
+
+                          'america':  ['Argentina', 'Bahamas', 'Brazil', 'Canada', 'Chile', 'Colombia',
+                                       'Costa Rica', 'Ecuador', 'Guatemala', 'Mexico', 'Panama', 'Peru', 'United States', 'Uruguay', 'Venez']
+                          }
