3a21d7a4115bb06d703c60ec16219863d3deb07f
Henrique Soares Frutuoso <ga87lay@mytum.de>
2020-06-25 11:33:42 +0200

    fixing pipeline

--- src/data_collection/Google_Trends_collector/Scrapper.py
+++ src/data_collection/Google_Trends_collector/Scrapper.py
@@ -1,13 +1,13 @@
-import pandas as pd                        
+                     
+import os
 from pytrends.request import TrendReq
-import os 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 cwd = os.getcwd()
 def get_keywords(keywords_file):
-    a = open(keywords_file)
+    kw_file = open(keywords_file)
     keywords = []
-    for line in a:
+    for line in kw_file:
         # "#" are supposed to be comments on txt file
         if line[0] != '#':
             keywords.append(line[:-1])
@@ -15,24 +15,21 @@ def get_keywords(keywords_file):
     return keywords
 
 def get_interest_over_time(kw_list):
-    x = list(divide_chunks(kw_list, 5)) 
+    kw_lists = list(divide_chunks(kw_list, 5))
     pytrend = TrendReq()
     # Interest by Region
-    counter = 1 
-    for kw_list in x:
-        pytrend.build_payload(kw_list = kw_list)
-        df = pytrend.interest_over_time()
-        
-        df.drop("isPartial",1,inplace=True) 
-        
-        df.to_csv("{}/Data/{}.csv".format(dir_path,counter))
-        counter+=1
+    counter = 1
+    for kw_list in kw_lists:
+        pytrend.build_payload(kw_list=kw_list)
+        trends_df = pytrend.interest_over_time()
+        trends_df.drop("isPartial",1,inplace=True)
+        trends_df.to_csv("{}/Data/{}.csv".format(dir_path, counter))
+        counter += 1
 
-def divide_chunks(list, n): 
-    # looping till length l 
-    for i in range(0, len(list), n):  
-        yield list[i:i + n] 
-
+def divide_chunks(list, size):
+    # looping till length l
+    for keyword in range(0, len(list), size):
+        yield list[keyword + size]
 
 kw_list = get_keywords("{}/keywords.txt".format(dir_path))
 get_interest_over_time(kw_list)
