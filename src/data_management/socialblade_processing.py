795bb0698bc3fe2b789d0eee8e5ea8de24a7fb30
Martin Schuck <martin.schuck@tum.de>
2020-06-25 16:40:51 +0200

    socialblade dataset V1.0

--- src/data_management/socialblade_processing.py
+++ src/data_management/socialblade_processing.py
@@ -1,5 +1,4 @@
 from pathlib import Path, PurePath
-import time
 import random
 from datetime import datetime
 import pandas as pd
@@ -11,14 +10,11 @@ import numpy as np
 
 
 def load_raw_data(data_path):
-    print('Loading raw data.')
     raw_data = list()
     data_files = [f for f in data_path.glob('*.json')]
-    t1 = time.time()
     for file in data_files:
         with file.open() as f:
             raw_data.extend(json.load(f))
-    print('Loading finished in {:.2f} seconds.'.format(time.time()-t1))
     return raw_data
 
 
@@ -32,32 +28,16 @@ def extract_key_list(raw_data):
 
 
 def sort_data(raw_data, key_list):
-    print('Sorting the data into unified dictionaries.')
-    data_dict = {key: [] for key in key_list}
+    data_dict = {key: [[], []] for key in key_list}
     for channel_dict in raw_data:
         for (key, val) in channel_dict.items():
             if val:  # Some data lists are empty and need to be removed.
                 val_x = list(map(int, val[0::2]))
+                val_x = [int(x/1000) for x in val_x]  # Times are in ms, should be s for conversion though.
                 val_y = list(map(int, val[1::2]))
-                data_dict[key].append([val_x, val_y])
+                data_dict[key][0].extend(val_x)
+                data_dict[key][1].extend(val_y)
     return data_dict
-
-
-def remove_false_data(data_dict):
-    print('Removing negative values from the data set.')
-    for (key, val) in data_dict.items():
-        if 'change' not in key:
-            for idx, data in enumerate(val):
-                val[idx] = smooth_out_negative_values(data)
-    return data_dict
-
-
-def smooth_out_negative_values(data):
-    y_data = data[1]
-    for idx, data_point in enumerate(y_data):
-        if data_point < 0:
-            y_data[idx] = y_data[idx-1] if not idx == 0 else 0
-    return data
 
 
 def show_data_subset(data_dict):
@@ -77,62 +57,80 @@ def show_data_subset(data_dict):
         plt.show()
 
 
-def merge_channel_data(data_dict):
-    merged_dict = dict()
-    for (key, val) in data_dict.items():
-        merged_dict[key] = list()
-        data_arr = get_data_array(key, val)
-        for arr in val:
-            for idx, el in enumerate(arr[0]):
-                if el not in t_dict[key]:
-                    t_dict[key].append(el)
-    return t_dict
+def create_month_df(data_dict, key, drop=False):
+    m_data = data_dict[key]
+    series = pd.Series(m_data[1], pd.to_datetime(m_data[0], unit='s'))
+    if drop:
+        series = series[series > 0]
+    series_rs = series.resample('1MS').sum()
+    series_cnt = series.resample('1MS').count()
+    df = pd.DataFrame({'Date': series_cnt.keys(), 'Views': series_rs.values, 'Accounts': series_cnt.values})
+    df.head()
+    return df
 
 
-def get_min_max_time(array):
-    t_min = 9999999999
-    t_max = 0
-    for sub_array in array:
-        t_min = np.min(sub_array[0]) if np.min(sub_array[0]) < t_min else t_min
-        t_max = np.max(sub_array[0]) if np.max(sub_array[0]) > t_max else t_max
-    return t_min, t_max
+def create_week_df(data_dict, key, drop=False):
+    m_data = data_dict[key]
+    series = pd.Series(m_data[1], pd.to_datetime(m_data[0], unit='s'))
+    if drop:
+        series = series[series > 0]
+    series_rs = series.resample('1W').sum()
+    series_cnt = series.resample('1W').count()
+    df = pd.DataFrame({'Date': series_cnt.keys(), 'Views': series_rs.values, 'Accounts': series_cnt.values})
+    df.head()
+    return df
 
 
-def get_data_array(key, val):
-    t_diff = 2592000 if 'monthly' in key else 604800
-    t_min, t_max = get_min_max_time(val)
-    t_max = np.round((t_max-t_min)/t_diff)*t_diff + t_min  # Get a matching discrete time step boundary.
-    data_array = np.array(size=(2, (t_max-t_min)/t_diff))
-    data_array[0] = np.arange(t_min, t_max + 1, t_diff)
-    return data_array
+def plot_df(df, title):
+    fig, ax = plt.subplots(figsize=(10, 10))
+    # Add x-axis and y-axis
+    sns.lineplot(df['Date'],
+                 df['Views']/df['Accounts'],
+                 color='purple')
+    # Set title and labels for axes
+    ax.set(xlabel="Date",
+           ylabel="Views",
+           title=title)
+    plt.show()
+
+
+def clean_df(df):
+    date_2018 = 1512000000  # Date of 1.1.2018 in UNIX seconds timestamp style.
+    idx = df[df['Date'].astype(np.int64) / 10 ** 9 < date_2018].index
+    df.drop(idx, inplace=True)
+    idx = df[df['Views'] == 0].index
+    df.drop(idx, inplace=True)
 
 
 def main():
-    raw_data = load_raw_data(PATH)
-    key_list = extract_key_list(raw_data)
-    data_dict = sort_data(raw_data, key_list)
-    df = pd.DataFrame.from_dict(raw_data)
-    df.head()
-    print('NO HEAD')
-    data_dict = remove_false_data(data_dict)
-    print('Press enter to inspect a new set, or any other key to finish.')
-    while input() == '':
-        show_data_subset(data_dict)
-        print('Press enter to inspect a new set, or any other key to finish.')
-    t_dict = merge_channel_data(data_dict)
-    with open('timestamps.json', 'w') as f:
-        json.dump(t_dict, f)
+    print('Loading raw data.')
+    raw_data = load_raw_data(PurePath.joinpath(PATH, 'raw'))
+    print('Sorting the data into unified dictionaries.')
+    data_dict = sort_data(raw_data, extract_key_list(raw_data))
+    df_list = list()
+    print('Creating pandas data frames.')
+    for key in data_dict.keys():
+        if 'change' in key:
+            df = create_week_df(data_dict, key, drop=False)
+        elif 'month' in key:
+            df = create_month_df(data_dict, key, True)
+        else:
+            df = create_week_df(data_dict, key, drop=True)
+        df.name = str(key)
+        df_list.append(df)
+    print('Cleaning data frames.')
+    for df in df_list:
+        clean_df(df)
+    print('Plotting data frames for inspection.')
+    for df in df_list:
+        plot_df(df, title=df.name)
+    print('Saving data frames into processed folder.')
+    for df in df_list:
+        df.to_csv(PurePath.joinpath(PATH, 'processed', df.name + '.csv').as_posix(), encoding='utf-8', index=False)
+    print('Saving complete, script will exit.')
 
 
 if __name__ == '__main__':
     sns.set(style="darkgrid")
-    PATH = PurePath.joinpath(Path(__file__).resolve().parents[2], 'res', 'socialblade', 'raw')
+    PATH = PurePath.joinpath(Path(__file__).resolve().parents[2], 'res', 'socialblade')
     main()
-    with open('timestamps.json', 'r') as f:
-        t_dict = json.load(f)
-    for key in t_dict.keys():
-        print(key)
-    arr = t_dict['daily-weekly-vidviews']
-    arr = sorted(arr)
-    plt.plot(arr)
-    plt.show()
