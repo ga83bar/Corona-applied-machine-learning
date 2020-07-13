fd1a639ff7a2ab8e45731888e471ea469cca7c13
Felix Montnacher <felix.montnacher@tum.de>
2020-07-13 21:52:36 +0200

    Modified file to fulfill flake8 format.

--- src/data_collection/DE_CIX_graph_analysation/de_cix_pre_processing.py
+++ src/data_collection/DE_CIX_graph_analysation/de_cix_pre_processing.py
@@ -1,3 +1,8 @@
+"""!@brief Script to reformat csv file of DE-CIX data according to the already used format for other IX in this project.
+@author Felix Montnacher
+@date 06.07.2020
+"""
+
 import csv
 from datetime import datetime
 from pathlib import Path
@@ -22,15 +27,15 @@ def process_file(csv_file_name, new_file_name, data_unit_factor, ix_name, ix_typ
 
     # Open existing and unprocessed CSV file
     raw_folder = Path("raw/{}".format(csv_file_name))
-    with open(raw_folder, 'rt') as rawCsvFile:
-        csv_reader = csv.reader(rawCsvFile)
+    with open(raw_folder, 'rt') as raw_csv_file:
+        csv_reader = csv.reader(raw_csv_file)
 
         # Open or create a new file
         # (Overwrites everything when file already exists)
         processed_folder = Path("processed/{}".format(new_file_name))
-        with open(processed_folder, 'wt', newline='') as newFile:
+        with open(processed_folder, 'wt', newline='') as new_file:
 
-            csv_writer = csv.writer(newFile)
+            csv_writer = csv.writer(new_file)
             # write header
             csv_writer.writerow(header)
 
@@ -45,24 +50,24 @@ def process_file(csv_file_name, new_file_name, data_unit_factor, ix_name, ix_typ
                     # Add the new items in the split_list to the newLine
                     formatted_raw_line += split_list
 
-                processedLine = list()
+                processed_line = list()
 
                 # Add the ID
-                processedLine.append(formatted_raw_line[0])
+                processed_line.append(formatted_raw_line[0])
                 # Add the Timestamp
                 date = datetime.strptime(formatted_raw_line[2], "%d.%m.%Y")
                 timestamp = datetime.timestamp(date)
-                processedLine.append(timestamp)
+                processed_line.append(timestamp)
                 # Add the Date
-                processedLine.append(date.date())
+                processed_line.append(date.date())
                 # Add the bitrate
-                processedLine.append(float(formatted_raw_line[1]) * data_unit_factor)
+                processed_line.append(float(formatted_raw_line[1]) * data_unit_factor)
                 # Add the IX
-                processedLine.append(ix_name)
+                processed_line.append(ix_name)
                 # Add the type
-                processedLine.append(ix_type)
+                processed_line.append(ix_type)
 
-                csv_writer.writerow(processedLine)
+                csv_writer.writerow(processed_line)
 
 
 process_file("DE-CIX Dusseldorf_1_year.csv", "ix_dusseldorf_1_year.csv", 10**9, "ix_dusseldorf", "incoming")
