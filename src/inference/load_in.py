a8cc81f826fcb4b336b630594f4e89f051d7022f
Alexander Griessel <ga94cav@mytum.de>
2020-08-11 17:49:01 +0200

    Cleaining up the dust bunnies

--- src/inference/load_in.py
+++ src/inference/load_in.py
@@ -1,19 +1,24 @@
+"""
+This module handles the loading and management of data for model training
+and model testing. Any and all functions that directly handle data for a learning
+task MUST go through here.
+"""
+
+import os
 import pandas as pd
 
 DATA_FILE = "res/{}/{}/{}.csv"
 DATA_DIRECTORY = "res/{}/{}"
 
-class Load_in():
-    """ 
-    This class handles the loading and management of data for model training
-    and model testing. Any and all functions that directly handle data for a learning
-    task MUST go through here.
+class LoadIn():
+    """
+    This class handles imports and persistent storage of datasets
     """
     def __init__(self):
         """
         Constructor. Initaliaze a loading manager.
         """
-        self.datasets={
+        self.datasets = {
             "app": "apple-mobility",
             "cov": "covid",
             "dec": "de-cix",
@@ -27,9 +32,16 @@ class Load_in():
             "ste": "steam",
             "twi": "twitch"
         }
-        self.dataframes={}
+        self.dataframes = {}
 
-    def get_set(self, strs=["cov"], typ="processed")
+    def get_set(self, strs, typ="processed"):
         """ Get dataframes for each dataset, load them """
+        dataframes = {}
         for dset in strs:
-            self.dataframes[dset] = pd.read_csv(DATA_FILE.format(self.datasets[dset], typ, name))
+            for file in os.listdir(DATA_DIRECTORY.format(self.datasets[dset], typ)):
+                if file.endswith(".csv") and not self.dataframes[file]:
+                    self.dataframes[file] = pd.read_csv(DATA_FILE.format(self.datasets[dset], typ, file))
+                    dataframes[file] = pd.read_csv(DATA_FILE.format(self.datasets[dset], typ, file))
+                if self.dataframes[file]:
+                    dataframes[file] = self.dataframes[file]
+        return dataframes
