a91e6d9103522ad0cf2e91b6c0ce33609715e682
Felix Montnacher <felix.montnacher@tum.de>
2020-08-11 13:39:13 +0200

    Added dynamic labels for graph

--- src/webinterface/backend/api.py
+++ src/webinterface/backend/api.py
@@ -19,16 +19,25 @@ class Predict(Resource):
     def post(self):
         args = parser.parse_args()  # Sklearn is VERY PICKY on how you put your values in...
 
-        print("request is processed")
+        print("\nrequest is processed")
         print(args["dataset_req"])
         print(args["start_date_req"])
         print(args["end_date_req"])
         if (args["dataset_req"] == '1'):
-            return {"class": 42, "chart_data_1": [5, 8, 2, 4, 6, 5, 8, 2, 4, 6], "chart_data_2": [5, 8, 2, 4, 6, 5, 7, 1, 3, 5]}
+            return {"class": 42,
+                    "chart_data_1": [5, 8, 2, 4, 6, 5, 8, 2, 4, 6, 8, 9],
+                    "chart_data_2": [5, 8, 2, 4, 6, 5, 7, 1, 3, 5],
+                    "labels": [1, 2, 3, 4, 5, 6, 7, 8, 9]}
         elif (args["dataset_req"] == '2'):
-            return {"class": 42, "chart_data_1": [5, 2, 3, 5, 6, 1, 2, 3, 5, 6], "chart_data_2": [5, 8, 2, 4, 6, 5, 8, 2, 4, 6]}
+            return {"class": 42,
+                    "chart_data_1": [5, 2, 3, 5, 6, 1, 2, 3, 5, 6],
+                    "chart_data_2": [5, 8, 2, 4, 6, 5, 8, 2, 4, 6],
+                    "labels": [1, 2, 3, 4, 5, 6, 7, 8, 9]}
         else:
-            return {"class": 500, "chart_data": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "chart_data_2": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+            return {"class": 500,
+                    "chart_data": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    "chart_data_2": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    "labels": [1, 2, 3, 4, 5, 6, 7, 8, 9]}
 
 
 api.add_resource(Predict, "/predict")
