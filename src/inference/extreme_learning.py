384e07fefa081e048b20be87bd15eb311bf8be26
ga83bar <brandnermichi@googlemail.com>
2020-08-21 13:40:16 +0200

    Elm works with scikit learn

--- src/inference/extreme_learning.py
+++ src/inference/extreme_learning.py
@@ -2,113 +2,37 @@
 File implemanting Extrem Learning Machine (ELM).
 I tried to make this class scjíkit learn compatibel
 (see :https://scikit-learn.org/stable/developers/develop.html)
+
+How to use this class:
+(1) Run fit before predict!
+(2) The input for the fit method are the stacked time series data!
+    As you get (for every attribute) from :
+    x_train_test, y_train_test = elm.prepare_data(loaded_data['stock_automotive'])
 '''
 from pathlib import Path
-from abc import ABCMeta, abstractmethod
 from keras import Sequential
 from keras.layers import Dense
 import pandas as pd
 import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
 
 # For scikitlearn cross validation
 from sklearn.model_selection import cross_val_score
-from sklearn.base import BaseEstimator
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 
-
-# Base class for extreme learning machine implements
-# scikit learn methods
-class BaseModel(metaclass=ABCMeta):
-    '''
-    Base model class for extrem learning machine and hopfully also for the other classes.
-    '''
-    def __init__(self, error_measure, train_window):
-        self.__error_measure = error_measure
-        self.train_window = train_window
-
-    @abstractmethod
-    def fit(self, x_train, y_train):
-        '''
-        Scikit learn base method that trains the model.
-        @param x_train : x values of the train data
-        @param y_train : y values of the train data
-        @return None
-        '''
-        raise NotImplementedError
-
-    @abstractmethod
-    def score(self, x_test, y_test):
-        '''
-        Scoreing method, that evaluates the test error and returns it!
-        '''
-        raise NotImplementedError
-
-    # Error measure dependent things
-    def get_error_measure(self):
-        '''
-        Getter method error measure
-        '''
-        return self.__error_measure
-
-    def set_error_measure(self, error_measure):
-        '''
-        Setter method for error measure.
-        @param error_measure : error measure e.g mse, mae
-        @return : boolean value indicates if we seted the value or not
-        '''
-        if isinstance(error_measure, str):
-            if (error_measure == 'mse' or error_measure == 'mae'):
-                self.__error_measure = error_measure
-                return True
-            else:
-                print('{} is no valid Error measure for ELM'.format(error_measure))
-                return False
-        else:
-            print('Your input is not even a string =(')
-            return False
-
-    def prepare_data(self, data):
-        '''
-        Shifting method that generates a x and y like dataset
-        out of a time series.
-        @param data: 1-D List containing the data.
-        '''
-        x_train_test = []
-        y_train_test = []
-
-        for iterator in range(self.train_window, len(data)):
-            x_train_test.append(data[iterator-self.train_window:iterator])
-            y_train_test.append(data[iterator])
-
-        return(x_train_test, y_train_test)
-
-    def mse(self, y_real, y_predict):
-        '''
-        Calculate mean squared error
-        '''
-        error = np.square(y_real - y_predict)
-        return error
-
-    def mae(self, y_real, y_predict):
-        '''
-        Calculate the Mean Absolute error
-        '''
-        error = np.sum(y_real -y_predict)
-        return error
-
-    # Prpoerties
-    error_measure = property(get_error_measure, set_error_measure)
-
+# Constants
+TRAIN_WINDOW = 30
 
 # Extreme Learning Class
-class ExtremeLearningMachine(BaseModel, BaseEstimator):
+class ExtremeLearningMachine(BaseEstimator, ClassifierMixin):
     '''
     Implementation of extreme Learning machine.
     As proposed in paper from Guang-Bin Huang.
+    Can be used like any scikit learn predictor.
     '''
 
-
-    def __init__(self, layers=1, neurons=128, metric='mean_squared_error',
-                 activation='relu', error_measure='mse', test_train_split=0.66, train_window=30):
+    def __init__(self, neurons=128, lambd=0.01):
         '''
         The params define the neural network!
         @param data : the dataset in sequential form
@@ -118,30 +42,8 @@ class ExtremeLearningMachine(BaseModel, BaseEstimator):
         @param train_window : number of inputs for the net
         @param error_measure : error measure e.g mse
         '''
-
-        # Call parent Constructor
-        super(ExtremeLearningMachine, self).__init__(error_measure=error_measure,
-                                                     train_window=train_window)
-
-        self.alph = 0.001
+        self.lambd = lambd
         self.neurons = neurons
-
-        self.train_window = train_window
-        self.train_test_split = test_train_split
-
-        # Init neural network
-        self.model = Sequential()
-        self.model.add(Dense(neurons,
-                             activation=activation,
-                             input_shape=(train_window, ),
-                             bias_initializer='glorot_uniform'))
-        for _ in range(1, layers):
-            self.model.add(Dense(neurons, activation=activation))
-        self.model.compile(loss=metric, optimizer='adam')
-        self.model._make_predict_function()
-
-        # Regression part
-        self.weights = None
 
     def fit(self, x_train, y_train):
         '''
@@ -149,6 +51,10 @@ class ExtremeLearningMachine(BaseModel, BaseEstimator):
         are calculated in the following method.
         We fit a single data attribute! But with the same nn!
         '''
+        # To be scikit learn comp we build the model outside the init method
+        self.build_network()
+        self.__measure = 'mse'
+
         transformed_features = self.model.predict(x_train)
         sol_eqs = np.linalg.lstsq(transformed_features, y_train, rcond=None)
         self.weights = sol_eqs[0]
@@ -159,7 +65,7 @@ class ExtremeLearningMachine(BaseModel, BaseEstimator):
         '''
         y_predict = self.predict(x_test)
 
-        if self.__error_measure == 'mse':
+        if self.__measure == 'mse':
             error = self.mse(y_predict, y_test)
         # Default value == MSE
         else:
@@ -176,21 +82,127 @@ class ExtremeLearningMachine(BaseModel, BaseEstimator):
         features = self.model.predict(x_vals)
         return np.matmul(features, self.weights)
 
+    def build_network(self, neurons=128, layers=1, train_window=TRAIN_WINDOW,
+                      activation='relu', metric='mean_squared_error'):
+        '''error_measure
+        Method that builds and compiles the model (network)
+        '''
+        # Init neural network
+        self.model = Sequential()
+        self.model.add(Dense(neurons,
+                             activation=activation,
+                             input_shape=(train_window,),
+                             bias_initializer='glorot_uniform'))
+        for _ in range(1, layers):
+            self.model.add(Dense(neurons, activation=activation))
+        self.model.compile(loss=metric, optimizer='adam')
+        self.model._make_predict_function()
+
+        # Regression part
+        self.weights = None
+
+    def prepare_data(self, data, train_window=TRAIN_WINDOW):
+        '''
+        Shifting method that generates a x and y like dataset
+        out of a time series.
+        @param data: 1-D List containing the data.
+        '''
+        x_train_test = []
+        y_train_test = []
+
+        for iterator in range(train_window, len(data)):
+            x_vals = data[(iterator-train_window):iterator]
+            x_train_test.append(x_vals)
+            y_train_test.append(data[iterator])
+
+        # cast this stuff to numpy
+        x_train_test = np.array(x_train_test)
+        y_train_test = np.array(y_train_test)
+
+        return x_train_test, y_train_test
+    # Error measure dependent things
+    def get_error_measure(self):
+        '''
+        Getter method error measure
+        '''
+        return self.__measure
+
+    def set_error_measure(self, error_measure):
+        '''
+        Setter method for error measure.
+        @param error_measure : error measure e.g mse, mae
+        @return : boolean value indicates if we seted the value or not
+        '''
+        if isinstance(__measure, str):
+            if (__measure == 'mse' or __measure == 'mae'):
+                self.__measure = error_measure
+                return True
+            else:
+                print('{} is no valid Error measure for ELM'.format(error_measure))
+                return False
+        else:
+            print('Your input is not even a string =(')
+            return False
+
+    def mse(self, y_real, y_predict):
+        '''
+        Calculate mean squared error
+        '''
+        difference_array = np. subtract(y_real, y_predict)
+        squared_array = np. square(difference_array)
+        mse = squared_array. mean()
+        return mse
+
+    def mae(self, y_real, y_predict):
+        '''
+        Calculate the Mean Absolute error
+        '''
+        error = np.sum(y_real -y_predict)
+        return error
+
+    # Prpoerties
+    error_measure = property(get_error_measure, set_error_measure)
+
 
 def test():
-    ''' DUMMY'''
+    ''' Test running sckikit learn cross validation'''
     root = Path().absolute().parent.parent
     dataset_path = root.joinpath('AML', 'group11', 'res', 'pipeline', 'scaled_corona_df.csv')
     loaded_data = pd.read_csv(dataset_path)
-
     elm = ExtremeLearningMachine()
 
-    x_train_test, y_train_test = elm.prepare_data(loaded_data['stock_automotive'])
+    # Test if it works as expected
+    my_train_attr = ['stock_automotive', 'stock_energy']
+    for attr in my_train_attr:
+        # First step prepare dat
+        x_train_test, y_train_test = elm.prepare_data(loaded_data[attr])
+        scores = cross_val_score(elm, x_train_test, y_train_test, cv=5)
+        print('scores ELM:')
+        print(scores)
 
-    scores = cross_val_score(elm, x_train_test, y_train_test, cv=5)
-    print('scores ELM:')
-    print(scores)
+def test_grid_search():
+    ''' Test running sckikit learn cross validation'''
+    root = Path().absolute().parent.parent
+    dataset_path = root.joinpath('AML', 'group11', 'res', 'pipeline', 'scaled_corona_df.csv')
+    loaded_data = pd.read_csv(dataset_path)
+    elm = ExtremeLearningMachine()
+
+    # Test if it works as expected
+    my_train_attr = ['stock_automotive', 'stock_energy']
+    for attr in my_train_attr:
+        print(elm.get_params())
+
+        # First step prepare dat
+        x_train_test, y_train_test = elm.prepare_data(loaded_data[attr])
+        # Set the parameters by cross-validation
+        tuned_parameters = {'neurons': [1, 10, 20],
+                            'lambd' : [0, 0.0001, 0.001, 0.01]},
+        clf = GridSearchCV(
+        ExtremeLearningMachine(), tuned_parameters)
+        clf.fit(x_train_test, y_train_test)
+        print(clf.best_params_)
 
 
 if __name__ == '__main__':
-    test()
+    # test()
+    test_grid_search()
