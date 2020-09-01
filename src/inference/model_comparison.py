902878af09668f7d413c65dcd1261a68610783c9
Martin Schuck <martin.schuck@tum.de>
2020-09-01 17:54:37 +0200

    fix

--- src/inference/model_comparison.py
+++ src/inference/model_comparison.py
@@ -19,6 +19,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+import joblib
 
 # import . extreme_learning as EM
 from src.inference import extreme_learning as EM
