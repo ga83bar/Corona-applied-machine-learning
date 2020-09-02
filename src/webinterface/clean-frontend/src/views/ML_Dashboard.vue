c9c15873e110dd1489e921df63af9c6156b24021
Felix Montnacher <felix.montnacher@tum.de>
2020-09-02 22:31:29 +0200

    Changed drop-down namings

--- src/webinterface/clean-frontend/src/views/ML_Dashboard.vue
+++ src/webinterface/clean-frontend/src/views/ML_Dashboard.vue
@@ -20,15 +20,15 @@
                         <md-button md-menu-trigger class="fixed-width-button">{{selectedDataset_graph1}}</md-button>
                         <md-menu-content>
                             <md-menu-item @click="dataset_id='0', selectedDataset_graph1='Internet Exchange Points'">Internet Exchange Points</md-menu-item>
-                            <md-menu-item @click="dataset_id='1', selectedDataset_graph1='youtube_viewchange'">youtube_viewchange</md-menu-item>
-                            <md-menu-item @click="dataset_id='2', selectedDataset_graph1='youtube_views'">youtube_views</md-menu-item>
-                            <md-menu-item @click="dataset_id='3', selectedDataset_graph1='steam_users'">steam_users</md-menu-item>
-                            <md-menu-item @click="dataset_id='4', selectedDataset_graph1='steam_ingame'">steam_ingame</md-menu-item>
-                            <md-menu-item @click="dataset_id='5', selectedDataset_graph1='twitch_views'">twitch_views</md-menu-item>
-                            <md-menu-item @click="dataset_id='6', selectedDataset_graph1='twitch_channels'">twitch_channels</md-menu-item>
-                            <md-menu-item @click="dataset_id='7', selectedDataset_graph1='twitch_viewtime'">twitch_viewtime</md-menu-item>
-                            <md-menu-item @click="dataset_id='8', selectedDataset_graph1='twitch_streams'">twitch_streams</md-menu-item>
-                            <md-menu-item @click="dataset_id='9', selectedDataset_graph1='ps_users'">ps_users</md-menu-item>
+                            <md-menu-item @click="dataset_id='1', selectedDataset_graph1='YouTube Viewchange'">YouTube Viewchange</md-menu-item>
+                            <md-menu-item @click="dataset_id='2', selectedDataset_graph1='YouTube Views'">YouTube Views</md-menu-item>
+                            <md-menu-item @click="dataset_id='3', selectedDataset_graph1='Steam Users'">Steam Users</md-menu-item>
+                            <md-menu-item @click="dataset_id='4', selectedDataset_graph1='Steam Ingame'">Steam Ingame</md-menu-item>
+                            <md-menu-item @click="dataset_id='5', selectedDataset_graph1='Twitch Views'">Twitch Views</md-menu-item>
+                            <md-menu-item @click="dataset_id='6', selectedDataset_graph1='Twitch Channels'">Twitch Channels</md-menu-item>
+                            <md-menu-item @click="dataset_id='7', selectedDataset_graph1='Twitch Viewtime'">Twitch Viewtime</md-menu-item>
+                            <md-menu-item @click="dataset_id='8', selectedDataset_graph1='Twitch Streams'">Twitch Streams</md-menu-item>
+                            <md-menu-item @click="dataset_id='9', selectedDataset_graph1='PlayStation Network Users'">PlayStation Network Users</md-menu-item>
                         </md-menu-content>
                     </md-menu>
                     <md-button class="md-success md-round run" @click='select_set(1)'>Run Inference</md-button>
@@ -95,14 +95,14 @@
                     <md-menu md-size="medium" md-align-trigger class="menuu">
                         <md-button md-menu-trigger class="fixed-width-button">{{selectedDataset_graph2}}</md-button>
                         <md-menu-content>
-                            <md-menu-item @click="dataset_id='10', selectedDataset_graph2='stock_med'">stock_med</md-menu-item>
-                            <md-menu-item @click="dataset_id='11', selectedDataset_graph2='stock_bank'">stock_bank</md-menu-item>
-                            <md-menu-item @click="dataset_id='12', selectedDataset_graph2='stock_energy'">stock_energy</md-menu-item>
-                            <md-menu-item @click="dataset_id='13', selectedDataset_graph2='stock_oil'">stock_oil</md-menu-item>
-                            <md-menu-item @click="dataset_id='14', selectedDataset_graph2='stock_steel'">stock_steel</md-menu-item>
-                            <md-menu-item @click="dataset_id='15', selectedDataset_graph2='stock_automotive'">stock_automotive</md-menu-item>
-                            <md-menu-item @click="dataset_id='16', selectedDataset_graph2='stock_telecom'">stock_telecom</md-menu-item>
-                            <md-menu-item @click="dataset_id='17', selectedDataset_graph2='stock_tech'">stock_tech</md-menu-item>
+                            <md-menu-item @click="dataset_id='10', selectedDataset_graph2='Medical Stock'">Medical Stock</md-menu-item>
+                            <md-menu-item @click="dataset_id='11', selectedDataset_graph2='Banking Stock'">Banking Stock</md-menu-item>
+                            <md-menu-item @click="dataset_id='12', selectedDataset_graph2='Energy Stock'">Energy Stock</md-menu-item>
+                            <md-menu-item @click="dataset_id='13', selectedDataset_graph2='Oil Stock'">Oil Stock</md-menu-item>
+                            <md-menu-item @click="dataset_id='14', selectedDataset_graph2='Steel Stock'">Steel Stock</md-menu-item>
+                            <md-menu-item @click="dataset_id='15', selectedDataset_graph2='Automotive Stock'">Automotive Stock</md-menu-item>
+                            <md-menu-item @click="dataset_id='16', selectedDataset_graph2='Telecom Stock'">Telecom Stock</md-menu-item>
+                            <md-menu-item @click="dataset_id='17', selectedDataset_graph2='Tech Stock'">Tech Stock</md-menu-item>
                         </md-menu-content>
                     </md-menu>
                     <md-button class="md-success md-round run" @click='select_set(2)'>Run Inference</md-button>
