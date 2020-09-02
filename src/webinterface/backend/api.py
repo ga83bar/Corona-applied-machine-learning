7a7373aa0772241ede4b8bb196d73ef4882ac593
Felix Montnacher <felix.montnacher@tum.de>
2020-09-02 12:05:49 +0200

    pylint conform

--- src/webinterface/backend/api.py
+++ src/webinterface/backend/api.py
@@ -1,17 +1,19 @@
+""" !@brief File to start backend and handle incoming requests """
+
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 import pandas as pd
 from src.inference.model_comparison import get_predict_data as predictor
 
-app = Flask(__name__)
-CORS(app)
+APP = Flask(__name__)
+CORS(APP)
 
-api = Api(app)  # Require a parser to parse our POST request.
-parser = reqparse.RequestParser()
-parser.add_argument("dataset_id_req")
-parser.add_argument("ping")
-parser.add_argument("selected_graph")
+API = Api(APP)  # Require a parser to parse our POST request.
+PARSER = reqparse.RequestParser()
+PARSER.add_argument("dataset_id_req")
+PARSER.add_argument("ping")
+PARSER.add_argument("selected_graph")
 # Unpickle our model so we can use it!
 
 
@@ -52,156 +54,200 @@ parser.add_argument("selected_graph")
 #     return data
 
 
-class switcher():
-    """
-    !@brief Implemets a switch-case function in python to select the right dataset name for a specified ID
-    @param Generate an instance of the switcher class and use the id of an dataset
-    as an id in the dataset_switch function
-    @return For the specified integer ID in the dataset_swicht function,
-    the name of the according dataset gets returned as a string
-    """
+class Switcher():
+    """ !@brief Implemets a switch-case function in python to select the right dataset name for a specified ID """
+
     def dataset_switch(self, dataset_id):
+        """
+        !@brief Is used to select the right name for a specific dataset id
+        @param Id of an dataset to recive the according name from
+        @return For the specified integer ID in the dataset_swicht function,
+        the name of the according dataset is returned as a string
+        """
+
         default = "incorrect dataset"
         # getattr returns function from within the class, matching to 'dataset_id'
         # lambda gets returned, if not matching class was found
         return getattr(self, 'dataset_' + str(dataset_id), lambda: default)()
 
-    def dataset_0(self):
+    @staticmethod
+    def dataset_0():
+        """ !@brief A switch case containing the datset name to an according id """
         return "ix_bitrate"
 
-    def dataset_1(self):
+    @staticmethod
+    def dataset_1():
+        """ !@brief A switch case containing the datset name to an according id """
         return "youtube_viewchange"
 
-    def dataset_2(self):
+    @staticmethod
+    def dataset_2():
+        """ !@brief A switch case containing the datset name to an according id """
         return "youtube_views"
 
-    def dataset_3(self):
+    @staticmethod
+    def dataset_3():
+        """ !@brief A switch case containing the datset name to an according id """
         return "steam_users"
 
-    def dataset_4(self):
+    @staticmethod
+    def dataset_4():
+        """ !@brief A switch case containing the datset name to an according id """
         return "steam_ingame"
 
-    def dataset_5(self):
+    @staticmethod
+    def dataset_5():
+        """ !@brief A switch case containing the datset name to an according id """
         return "twitch_views"
 
-    def dataset_6(self):
+    @staticmethod
+    def dataset_6():
+        """ !@brief A switch case containing the datset name to an according id """
         return "twitch_channels"
 
-    def dataset_7(self):
+    @staticmethod
+    def dataset_7():
+        """ !@brief A switch case containing the datset name to an according id """
         return "twitch_viewtime"
 
-    def dataset_8(self):
+    @staticmethod
+    def dataset_8():
+        """ !@brief A switch case containing the datset name to an according id """
         return "twitch_streams"
 
-    def dataset_9(self):
+    @staticmethod
+    def dataset_9():
+        """ !@brief A switch case containing the datset name to an according id """
         return "ps_users"
 
-    def dataset_10(self):
+    @staticmethod
+    def dataset_10():
+        """ !@brief A switch case containing the datset name to an according id """
         return "stock_med"
 
-    def dataset_11(self):
+    @staticmethod
+    def dataset_11():
+        """ !@brief A switch case containing the datset name to an according id """
         return "stock_bank"
 
-    def dataset_12(self):
+    @staticmethod
+    def dataset_12():
+        """ !@brief A switch case containing the datset name to an according id """
         return "stock_energy"
 
-    def dataset_13(self):
+    @staticmethod
+    def dataset_13():
+        """ !@brief A switch case containing the datset name to an according id """
         return "stock_oil"
 
-    def dataset_14(self):
+    @staticmethod
+    def dataset_14():
+        """ !@brief A switch case containing the datset name to an according id """
         return "stock_steel"
 
-    def dataset_15(self):
+    @staticmethod
+    def dataset_15():
+        """ !@brief A switch case containing the datset name to an according id """
         return "stock_automotive"
 
-    def dataset_16(self):
+    @staticmethod
+    def dataset_16():
+        """ !@brief A switch case containing the datset name to an according id """
         return "stock_telecom"
 
-    def dataset_17(self):
+    @staticmethod
+    def dataset_17():
+        """ !@brief A switch case containing the datset name to an according id """
         return "stock_tech"
 
 
 class Predict(Resource):
-    def post(self):
+    """
+    !@brief Is used to recive incoming requests, process the request and
+    respond with an according response message
+    """
+
+    @staticmethod
+    def post():
         """
         !@brief Is getting called, when a http request has been received.
         The content gets extracted and either a ping-return is sent or a ML-Model is loaded
         and predicts data for a requested dataset, which will be returned.
         """
 
-        args = parser.parse_args()  # Sklearn is VERY PICKY on how you put your values in...
+        args = PARSER.parse_args()  # Sklearn is VERY PICKY on how you put your values in...
 
         print("\n---New request is processed---")
         print("Args: {}".format(args))
 
-        if (args["ping"] == "1"):
+        if args["ping"] == "1":
             print("answered keep-alive")
             return {"alive": 1}
-        else:
-            # Print for checking parameter during development
-            print("Received 'selected_graph' paramter: {}".format(args["selected_graph"]))
 
-            # Switch statement depending on the requested dataset to reiceive the dataset name out of it´s ID.
-            # The allocation from the id to the lable (name) can be found in /.../allocation_datasets_id.CSV
-            switch = switcher()
-            dataset_lable = switch.dataset_switch(args["dataset_id_req"])
+        # Print for checking parameter during development
+        print("Received 'selected_graph' paramter: {}".format(args["selected_graph"]))
 
-            # Use function to predict values and get raw and predicted data returned
-            predicted_df, prophet_attr_df_post,  prophet_attr_df_pre = predictor(dataset_lable)
+        # Switch statement depending on the requested dataset to reiceive the dataset name out of it´s ID.
+        # The allocation from the id to the lable (name) can be found in /.../allocation_datasets_id.CSV
+        dataset_lable = Switcher().dataset_switch(args["dataset_id_req"])
 
-            # Unpredicted data is processed
-            # Received data is merged together with data up to and after the 1.1.2020.
-            # Drop last row to not use it twice
-            prophet_attr_df_pre = prophet_attr_df_pre.drop(prophet_attr_df_pre.index[-1])
-            datasets_unpredicted = [prophet_attr_df_pre, prophet_attr_df_post]
-            result_unpredicted = pd.concat(datasets_unpredicted)
-            result_unpredicted.reset_index(inplace=True)
+        # Use function to predict values and get raw and predicted data returned
+        predicted_df, prophet_attr_df_post, prophet_attr_df_pre = predictor(dataset_lable)
 
-            # Get the index of the 2020-01-01 (where the rpediction starts)
-            index_prediction_start = result_unpredicted.loc[result_unpredicted["Date"] == "2020-01-01"].index.item()
+        # Unpredicted data is processed
+        # Received data is merged together with data up to and after the 1.1.2020.
+        # Drop last row to not use it twice
+        prophet_attr_df_pre = prophet_attr_df_pre.drop(prophet_attr_df_pre.index[-1])
+        datasets_unpredicted = [prophet_attr_df_pre, prophet_attr_df_post]
+        result_unpredicted = pd.concat(datasets_unpredicted)
+        result_unpredicted.reset_index(inplace=True)
 
-            # Predicted data processed
-            # Change data type into pandas series
-            result_predicted = pd.Series(predicted_df)
+        # Get the index of the 2020-01-01 (where the rpediction starts)
+        index_prediction_start = result_unpredicted.loc[result_unpredicted["Date"] == "2020-01-01"].index.item()
 
-            # Generate lists out of dataframes to send to frontend
-            complete_unpredicted_data = result_unpredicted[dataset_lable].to_list()
-            complete_model_data = result_predicted.to_list()
-            complete_predicted_data = result_predicted.to_list()
-            result_dates = result_unpredicted["Date"].to_list()
+        # Predicted data processed
+        # Change data type into pandas series
+        result_predicted = pd.Series(predicted_df)
 
-            for i in range(index_prediction_start, len(complete_model_data)):
-                complete_model_data[i] = None
+        # Generate lists out of dataframes to send to frontend
+        complete_unpredicted_data = result_unpredicted[dataset_lable].to_list()
+        complete_model_data = result_predicted.to_list()
+        complete_predicted_data = result_predicted.to_list()
+        result_dates = result_unpredicted["Date"].to_list()
 
-            for i in range(0, index_prediction_start - 1):
-                complete_predicted_data[i] = None
+        for index in range(index_prediction_start, len(complete_model_data)):
+            complete_model_data[index] = None
 
-            # The return statement will be sent to the frontend.
-            return {"class": 42,
-                    "datecheck": 2,
-                    "chart_data_1": complete_unpredicted_data,
-                    "chart_data_2": complete_model_data,
-                    "chart_data_3": complete_predicted_data,
-                    "labels": result_dates,
-                    "selected_graph": args["selected_graph"]
-                    }
+        for index in range(0, index_prediction_start - 1):
+            complete_predicted_data[index] = None
 
-            # TODO: Catch case when no data returned?
-            # else:
-            #     return {"class": 500,
-            #             "datecheck": 2,
-            #             "chart_data": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            #             "chart_data_2": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            #             "labels": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            #             }
+        # The return statement will be sent to the frontend.
+        return {"class": 42,
+                "datecheck": 2,
+                "chart_data_1": complete_unpredicted_data,
+                "chart_data_2": complete_model_data,
+                "chart_data_3": complete_predicted_data,
+                "labels": result_dates,
+                "selected_graph": args["selected_graph"]
+                }
+
+        # TODO: Catch case when no data returned?
+        # else:
+        #     return {"class": 500,
+        #             "datecheck": 2,
+        #             "chart_data": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        #             "chart_data_2": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        #             "labels": [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        #             }
 
 
-api.add_resource(Predict, "/predict")
+API.add_resource(Predict, "/predict")
 
 
 def main():
+    """ !@brief Main function """
     print("Starting server.")
-    app.run(debug=True, host="0.0.0.0")
+    APP.run(debug=True, host="0.0.0.0")
 
 
 if __name__ == "__main__":
